75bc5c90f2fd187642ac64257b34214c81f38ce8
Michal Jirák <77152961+MichalJirak@users.noreply.github.com>
2026-05-11 10:02:30 +0200

    funkncni sitemap a robots linky

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      disallow: ['/admin', '/dashboard', '/api'],
+    },
+    sitemap: 'https://jogaseliskou.cz/sitemap.xml',
+  }
+}
